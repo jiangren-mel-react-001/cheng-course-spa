0ba20ffc8ad2bcef37d88d190ddcb9045bf90475
Cheng Chang <cheng.chang@team.telstra.com>
2018-02-01 19:47:43 +1100

    copy the react-bootstrap navbar and react-router examples

--- src/app/App.js
+++ src/app/App.js
@@ -1,14 +1,99 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import { MenuItem, Nav, NavItem, Navbar, NavDropdown } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+import { LinkContainer } from 'react-router-bootstrap';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        hello
-      </div>
+      <Router>
+        <div>
+          <Navbar inverse collapseOnSelect>
+            <Navbar.Header>
+              <Navbar.Brand>
+                <a href="#brand">Jiangren</a>
+              </Navbar.Brand>
+              <Navbar.Toggle />
+            </Navbar.Header>
+            <Navbar.Collapse>
+              <Nav>
+                <LinkContainer to="/about">
+                  <NavItem>About</NavItem>
+                </LinkContainer>
+                <LinkContainer to="/topic">
+                  <NavItem>Topic</NavItem>
+                </LinkContainer>
+              </Nav>
+              <Nav pullRight>
+                <NavItem eventKey={1} href="#">
+                  Login
+                </NavItem>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/topics" component={Topics} />
+        </div>
+      </Router>
     );
   }
 }
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+)
+
+const Topics = ({ match }) => (
+  <div>
+    <h2>Topics</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/rendering`}>
+          Rendering with React
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/components`}>
+          Components
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>
+          Props v. State
+        </Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.url}/:topicId`} component={Topic} />
+    <Route exact path={match.url} render={() => (
+      <h3>Please select a topic.</h3>
+    )} />
+  </div>
+)
+
+const Topic = ({ match }) => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+)
 
 export default App;
