13919903eb322fe4eaa9b21029faf53ff08c6b97
Cheng Chang <cheng.chang@team.telstra.com>
2018-05-21 12:14:19 +1000

    Fix a build issue

--- src/app/courses/CourseItem.js
+++ src/app/courses/CourseItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default (props) => (
     <div className="thumbnail">
-        <img  src={props.item.image} alt="photo"  width="300" height="200" />
+        <img  src={props.item.image} alt="member"  width="300" height="200" />
         <h2>{props.item.name}</h2>
         <p>{props.item.description}</p>
         <button className="btn btn-primary" onClick={() => props.onDetail(props.item)}>
